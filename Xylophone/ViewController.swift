8dacaaa6d218d35a602c490727567cf0e4a62848
Bojan Peric <bojan.peric@ev-box.com>
2019-12-02 16:35:57 +0100

    Project updated and ended
    Sounds and fade out/in animation whille typing tones added.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+ 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let note: String = sender.currentTitle!
+        playSound(soundName: note)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+        
+    }
     
-
+    func playSound(soundName: String) {
+        // Bundle.main -> (it will search for "c.wav" in) area of disk where our app's resources are
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
